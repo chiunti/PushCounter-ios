eb43aefffc12cf096bbaa6186a5d4db8b51b61c4
Chiunti <chiunti.abc@gmail.com>
2015-01-27 23:38:15 -0600

    Initial Commit

---
+++ pushCounter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pushCounter
+//
+//  Created by Chiunti on 27/01/15.
+//  Copyright (c) 2015 Chiunti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
